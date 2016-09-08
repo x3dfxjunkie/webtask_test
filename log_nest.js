143eb2e36266232592d7e8a59ff8497bc41def6d
Russell Anderson <x3dfxjunkie@gmail.com>
2016-09-08 02:19:15 +0000

    renamed to better reflect use

--- log_nest.js
+++ log_nest.js
@@ -1,10 +1,13 @@
 var MongoClient = require('mongodb').MongoClient;
 var Request = require('request');
 
-module.exports = function (wu_api_key, temp, bail) {
+module.exports = function (data, bail) {
+    // Pass the api key in with the request.
     var WeatherUnderground = 'https://api.wunderground.com/api/' + wu_api_key + '/conditions/q/zmw:23456.1.99999.json';
+
     Request(WeatherUnderground, function (error, response, body) {
       if (!error && response.statusCode == 200) {
+        // parse the json response from WU api
         var weather = JSON.parse(body).current_observation;
         console.log(weather.relative_humidity);
         console.log(weather.temp_f);
