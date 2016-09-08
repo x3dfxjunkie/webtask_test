ddff072bd25128bed0ecfa87621f38b17b323ccd
Russell Anderson <russell.anderson@homes.com>
2016-09-07 22:12:59 -0400

    WU is working with the info i want.

--- set-temp.js
+++ set-temp.js
@@ -5,7 +5,11 @@ module.exports = function (wu_api_key, temp, bail) {
     var WeatherUnderground = 'https://api.wunderground.com/api/' + wu_api_key + '/conditions/q/zmw:23456.1.99999.json';
     Request(WeatherUnderground, function (error, response, body) {
       if (!error && response.statusCode == 200) {
-        var weather = JSON.parse(body)
+        var weather = JSON.parse(body).current_observation;
+        console.log(weather.relative_humidity);
+        console.log(weather.temp_f);
+        console.log(weather.pressure_in);
+        console.log(weather.weather);
       }
     })
 }
